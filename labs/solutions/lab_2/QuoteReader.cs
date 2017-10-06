5f95b3d7b4215e4c59c0cef33ddad41ee79a6212
Jack Neilson <jackdneilson@gmail.com>
2017-10-06 11:25:32 +0100

    Added solution to lab 2

---
+++ labs/solutions/lab_2/QuoteReader.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace lab_2 {
+    public class QuoteReader {
+        public QuoteReader() {
+            RunTeleprompter().Wait();
+        }
+
+        private async Task ShowTeleprompter(TeleprompterConfig config) {
+            IEnumerable<string> words = ReadFrom("sampleQuotes.txt");
+            foreach (var line in words) {
+                Console.Write(line);
+                if (!string.IsNullOrWhiteSpace(line)) {
+                    await Task.Delay(config.DelayInMilliseconds);
+                }
+            }
+            config.SetDone();
+        }
+
+        private async Task GetInput(TeleprompterConfig config) {
+            Action work = () => {
+                do {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    if (key.KeyChar == '>') {
+                        config.UpdateDelay(-10);
+                    }
+                    else if (key.KeyChar == '<') {
+                        config.UpdateDelay(10);
+                    }
+                } while (!config.Done);
+            };
+            await Task.Run(work);
+        }
+        
+        IEnumerable<string> ReadFrom(string file) {
+            string line;
+            using (StreamReader reader = File.OpenText(file)) {
+                while ((line = reader.ReadLine()) != null) {
+                    String[] words = line.Split(' ');
+                    foreach (string word in words) {
+                        yield return word + " ";
+                        int lineLength = word.Length + 1;
+                        if (lineLength > 70) {
+                            yield return Environment.NewLine;
+                            lineLength = 0;
+                        }
+                    }
+                    yield return Environment.NewLine;
+                }
+            }
+        }
+        
+        private async Task RunTeleprompter() {
+            var config = new TeleprompterConfig();
+            var displayTask = ShowTeleprompter(config);
+            var speedTask = GetInput(config);
+            await Task.WhenAny(displayTask, speedTask);
+        }
+    }    
+}
---
+++ labs/solutions/lab_2/TeleprompterConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using static System.Math;
+
+namespace lab_2 {
+    internal class TeleprompterConfig {
+        public bool Done => done;
+        private bool done = false;
+        
+        private object lockHandle = new object();
+        public int DelayInMilliseconds { get; private set; } = 200;
+
+        public void UpdateDelay(int increment) {
+            int newDelay = Min(DelayInMilliseconds + increment, 1000);
+            newDelay = Max(newDelay, 20);
+            lock (lockHandle) {
+                DelayInMilliseconds = newDelay;
+            }
+        }
+
+        public void SetDone() {
+            done = true;
+        }
+    }
+}
